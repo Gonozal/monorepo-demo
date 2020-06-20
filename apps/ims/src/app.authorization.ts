9faba30b10a97d69c24d174784bc4c5f9bf386b4
Sören Link <not-my-mail@example.com>
2020-06-20 11:22:17 +0200

    feat(ims): added base allow and deny authroization rules

--- apps/ims/src/app.authorization.ts
+++ apps/ims/src/app.authorization.ts
@@ -3,6 +3,18 @@ import { rule } from '@monorepo/graphql/authentication-directive';
 
 export const authenticated = rule<unknown, unknown, Context>({
   cacheStrategy: 'contextual',
-})(async (parent, args, context, info) => {
+})(async (parent, args, context) => {
   return Boolean(context.user);
+});
+
+export const allow = rule<unknown, unknown, Context>({
+  cacheStrategy: 'contextual',
+})(async () => {
+  return true;
+});
+
+export const deny = rule<unknown, unknown, Context>({
+  cacheStrategy: 'contextual',
+})(async () => {
+  return false;
 });
